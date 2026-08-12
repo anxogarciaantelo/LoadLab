a0052843bd7ac1ddb5c711d3aceac57badd691e7
anxogarciaantelo <anxogarciaantelo@gmail.com>
2026-08-12 12:38:10 +0200

    Update 2_Plantilla.py

--- pages/2_Plantilla.py
+++ pages/2_Plantilla.py
@@ -11,6 +11,11 @@ from utils.math_helpers import *
 from utils.pdf_generator import *
 from database.db_manager import *
 
+# --- COMPROBACIÓN DE SEGURIDAD ---
+if not st.session_state.get("autenticado", False) or not st.session_state.get("equipo_seleccionado", False):
+    st.warning("Por favor, inicia sesión y selecciona un equipo primero.")
+    st.stop()
+
     if "vista_plantilla" not in st.session_state:
         st.session_state.vista_plantilla = "📋 Plantilla"
 
