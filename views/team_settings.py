bd3fa8f1691109f021d231ef09769270060d9cc2
anxogarciaantelo <anxogarciaantelo@gmail.com>
2026-08-18 20:14:53 +0200

    Update team_settings.py

--- views/team_settings.py
+++ views/team_settings.py
@@ -62,63 +62,77 @@ def render_panel_principal():
             cfg_g = st.session_state.config_mapeo["gps"]
             
             with t_well:
+                st.markdown("**Identificadores**")
+                c_w01, c_w02 = st.columns(2)
+                n_w_nombre = c_w01.text_input("Columna de Nombre (Wellness):", value=cfg_w.get("nombre", "Nombre"))
+                n_w_fecha = c_w02.text_input("Columna de Fecha (Wellness):", value=cfg_w.get("fecha", "Marca temporal"))
+                
+                st.markdown("---")
                 st.markdown("**Columnas del Cuestionario de Bienestar**")
                 c_w1, c_w2 = st.columns(2)
-                n_tqr = c_w1.text_input("TQR (Recuperación):", value=cfg_w["tqr"])
-                n_fatiga = c_w1.text_input("Fatiga:", value=cfg_w["fatiga"])
-                n_sueno = c_w1.text_input("Sueño:", value=cfg_w["sueno"])
-                n_dolor = c_w2.text_input("Dolor Muscular:", value=cfg_w["dolor"])
-                n_estres = c_w2.text_input("Estrés:", value=cfg_w["estres"])
-                n_humor = c_w2.text_input("Humor/Ánimo:", value=cfg_w["humor"])
+                n_tqr = c_w1.text_input("TQR (Recuperación):", value=cfg_w.get("tqr", "TQR"))
+                n_fatiga = c_w1.text_input("Fatiga:", value=cfg_w.get("fatiga", "W_Fatiga"))
+                n_sueno = c_w1.text_input("Sueño:", value=cfg_w.get("sueno", "W_Sueño"))
+                n_dolor = c_w2.text_input("Dolor Muscular:", value=cfg_w.get("dolor", "W_Dolor"))
+                n_estres = c_w2.text_input("Estrés:", value=cfg_w.get("estres", "W_Estres"))
+                n_humor = c_w2.text_input("Humor/Ánimo:", value=cfg_w.get("humor", "W_Humor"))
 
             with t_rpe:
+                st.markdown("**Identificadores**")
+                c_r01, c_r02 = st.columns(2)
+                n_r_nombre = c_r01.text_input("Columna de Nombre (RPE):", value=cfg_r.get("nombre", "Nombre"))
+                n_r_fecha = c_r02.text_input("Columna de Fecha (RPE):", value=cfg_r.get("fecha", "Marca temporal"))
+                
+                st.markdown("---")
                 st.markdown("**Columnas de Carga Interna**")
                 c_r1, c_r2 = st.columns(2)
-                n_min = c_r1.text_input("Minutos de Sesión:", value=cfg_r["minutos"])
-                n_rpe = c_r2.text_input("RPE (Esfuerzo):", value=cfg_r["rpe"])
+                n_min = c_r1.text_input("Minutos de Sesión:", value=cfg_r.get("minutos", "MIN"))
+                n_rpe = c_r2.text_input("RPE (Esfuerzo):", value=cfg_r.get("rpe", "RPE"))
 
             with t_gps1:
+                st.markdown("**Identificadores**")
+                c_g01, c_g02 = st.columns(2)
+                n_g_nombre = c_g01.text_input("Columna de Nombre (GPS):", value=cfg_g.get("nombre", "Player Name"))
+                n_g_fecha = c_g02.text_input("Columna de Fecha (GPS):", value=cfg_g.get("fecha", "Activity Date"))
+                
+                st.markdown("---")
                 st.markdown("**Métricas Generales y Alta Intensidad**")
                 c_g1, c_g2, c_g3 = st.columns(3)
-                n_min_gps = c_g1.text_input("Minutos GPS:", value=cfg_g["min_gps"])
-                n_dis = c_g1.text_input("Distancia Total:", value=cfg_g["dis"])
-                n_vmed = c_g2.text_input("Velocidad Media:", value=cfg_g["v_med"])
-                n_vmax = c_g2.text_input("Velocidad Máxima:", value=cfg_g["v_max"])
-                n_hid21 = c_g3.text_input("HSR (>21 km/h):", value=cfg_g["hid_21"])
-                n_hid24 = c_g3.text_input("Sprint (>24 km/h):", value=cfg_g["hid_24"])
-                n_spr24 = c_g3.text_input("Nº Sprints (>24):", value=cfg_g["spr_24"])
-                n_spr27 = c_g3.text_input("Nº Sprints (>27):", value=cfg_g["spr_27"])
+                n_min_gps = c_g1.text_input("Minutos GPS:", value=cfg_g.get("min_gps", "Time Played"))
+                n_dis = c_g1.text_input("Distancia Total:", value=cfg_g.get("dis", "Distance (km)"))
+                n_vmed = c_g2.text_input("Velocidad Media:", value=cfg_g.get("v_med", "Avg Speed (km/h)"))
+                n_vmax = c_g2.text_input("Velocidad Máxima:", value=cfg_g.get("v_max", "Max Speed (km/h)"))
+                n_hid21 = c_g3.text_input("HSR (>21 km/h):", value=cfg_g.get("hid_21", "HID distance (> 21.00 km/h)"))
+                n_hid24 = c_g3.text_input("Sprint (>24 km/h):", value=cfg_g.get("hid_24", "HID distance (> 24.00 km/h)"))
+                n_spr24 = c_g3.text_input("Nº Sprints (>24):", value=cfg_g.get("spr_24", "# of Sprints (> 24.00 km/h)"))
+                n_spr27 = c_g3.text_input("Nº Sprints (>27):", value=cfg_g.get("spr_27", "# of Sprints (> 30.00 km/h)"))
 
             with t_gps2:
                 st.markdown("**Rangos de Velocidad y Acelerometría**")
                 c_g4, c_g5, c_g6 = st.columns(3)
-                
-                # Aceleraciones y Deceleraciones
-                n_acc_max = c_g4.text_input("Aceleración Máx:", value=cfg_g["acc_max"])
-                n_acc2 = c_g4.text_input("ACC (>2 m/s²):", value=cfg_g["acc_2"])
-                n_acc3 = c_g4.text_input("ACC (>3 m/s²):", value=cfg_g["acc_3"])
-                n_acc4 = c_g4.text_input("ACC (>4 m/s²):", value=cfg_g["acc_4"])
-                
-                n_dcc2 = c_g5.text_input("DCC (>2 m/s²):", value=cfg_g["dcc_2"])
-                n_dcc3 = c_g5.text_input("DCC (>3 m/s²):", value=cfg_g["dcc_3"])
-                n_dcc4 = c_g5.text_input("DCC (>4 m/s²):", value=cfg_g["dcc_4"])
-                
-                # Rangos de velocidad
-                n_r0_7 = c_g6.text_input("Rango 0-7 km/h:", value=cfg_g["r_0_7"])
-                n_r7_14 = c_g6.text_input("Rango 7-14 km/h:", value=cfg_g["r_7_14"])
-                n_r14_21 = c_g6.text_input("Rango 14-21 km/h:", value=cfg_g["r_14_21"])
-                n_r21_24 = c_g6.text_input("Rango 21-24 km/h:", value=cfg_g["r_21_24"])
-                n_r24_27 = c_g6.text_input("Rango 24-27 km/h:", value=cfg_g["r_24_27"])
-                n_r27_30 = c_g6.text_input("Rango 27-30 km/h:", value=cfg_g["r_27_30"])
-                n_r30_45 = c_g6.text_input("Rango >30 km/h:", value=cfg_g["r_30_45"])
+                n_acc_max = c_g4.text_input("Aceleración Máx:", value=cfg_g.get("acc_max", "ACC. MÁXIMA"))
+                n_acc2 = c_g4.text_input("ACC (>2 m/s²):", value=cfg_g.get("acc_2", "# of Accelerations (> 2.00 m/s²)"))
+                n_acc3 = c_g4.text_input("ACC (>3 m/s²):", value=cfg_g.get("acc_3", "# of Accelerations (> 3.00 m/s²)"))
+                n_acc4 = c_g4.text_input("ACC (>4 m/s²):", value=cfg_g.get("acc_4", "# of Accelerations (> 4.00 m/s²)"))
+                n_dcc2 = c_g5.text_input("DCC (>2 m/s²):", value=cfg_g.get("dcc_2", "# of Decelerations (> 2.00 m/s²)"))
+                n_dcc3 = c_g5.text_input("DCC (>3 m/s²):", value=cfg_g.get("dcc_3", "# of Decelerations (> 3.00 m/s²)"))
+                n_dcc4 = c_g5.text_input("DCC (>4 m/s²):", value=cfg_g.get("dcc_4", "# of Decelerations (> 4.00 m/s²)"))
+                n_r0_7 = c_g6.text_input("Rango 0-7 km/h:", value=cfg_g.get("r_0_7", "Distance Speed Range (0 - 7 km)"))
+                n_r7_14 = c_g6.text_input("Rango 7-14 km/h:", value=cfg_g.get("r_7_14", "Distance Speed Range (7 - 14 km)"))
+                n_r14_21 = c_g6.text_input("Rango 14-21 km/h:", value=cfg_g.get("r_14_21", "Distance Speed Range (14 - 21 km)"))
+                n_r21_24 = c_g6.text_input("Rango 21-24 km/h:", value=cfg_g.get("r_21_24", "Distance Speed Range (21 - 24 km)"))
+                n_r24_27 = c_g6.text_input("Rango 24-27 km/h:", value=cfg_g.get("r_24_27", "Distance Speed Range (24 - 27 km)"))
+                n_r27_30 = c_g6.text_input("Rango 27-30 km/h:", value=cfg_g.get("r_27_30", "Distance Speed Range (27 - 30 km)"))
+                n_r30_45 = c_g6.text_input("Rango >30 km/h:", value=cfg_g.get("r_30_45", "Distance Speed Range (30 - 45 km)"))
 
             btn_mapeo = st.form_submit_button("💾 Guardar Plantilla de Importación", use_container_width=True)
             
             if btn_mapeo:
                 st.session_state.config_mapeo = {
-                    "wellness": {"tqr": n_tqr, "fatiga": n_fatiga, "sueno": n_sueno, "dolor": n_dolor, "estres": n_estres, "humor": n_humor},
-                    "rpe": {"minutos": n_min, "rpe": n_rpe},
+                    "wellness": {"nombre": n_w_nombre, "fecha": n_w_fecha, "tqr": n_tqr, "fatiga": n_fatiga, "sueno": n_sueno, "dolor": n_dolor, "estres": n_estres, "humor": n_humor},
+                    "rpe": {"nombre": n_r_nombre, "fecha": n_r_fecha, "minutos": n_min, "rpe": n_rpe},
                     "gps": {
+                        "nombre": n_g_nombre, "fecha": n_g_fecha,
                         "min_gps": n_min_gps, "dis": n_dis, "v_med": n_vmed, "v_max": n_vmax,
                         "hid_21": n_hid21, "hid_24": n_hid24, "spr_24": n_spr24, "spr_27": n_spr27,
                         "acc_max": n_acc_max, "acc_2": n_acc2, "acc_3": n_acc3, "acc_4": n_acc4,
